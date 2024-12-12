aa1dee7a7f1ace957718d614ff52ba465db4077d
Steve-xmh <39523898+Steve-xmh@users.noreply.github.com>
2024-12-12 18:20:42 +0800

    fixed macOS appearance

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,8 +1,29 @@
+use tauri::Manager;
+use tauri_plugin_decorum::WebviewWindowExt;
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_os::init())
         .plugin(tauri_plugin_shell::init())
+        .setup(|app| {
+            let main_window = app.get_webview_window("main").unwrap();
+            main_window.create_overlay_titlebar().unwrap();
+
+            #[cfg(target_os = "macos")]
+            {
+                main_window.set_traffic_lights_inset(16.0, 20.0).unwrap();
+                main_window.make_transparent().unwrap();
+                let main_window_clone = main_window.clone();
+                main_window.on_window_event(move |evt| {
+                    if let tauri::WindowEvent::Resized(_) = evt {
+                        main_window_clone.set_traffic_lights_inset(16.0, 20.0).unwrap();
+                    }
+                });
+            }
+
+            Ok(())
+        })
         .invoke_handler(tauri::generate_handler![])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
